70d11aae1ba34a52a8982ca012525b09701734d3
meikaya <meikayana@icloud.com>
2025-12-06 14:29:17 +0900

    swiperの部分がうまく動かなかったから修正

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,23 @@
 'use strict';
 
 const swiper = new Swiper(".swiper", {
-  centeredSlides: true, // 1枚目のスライドを中央にする
-  loop: true, // ループさせる
-  speed: 1500, // 少しゆっくり(デフォルトは300)
-  slidesPerView: 3.5, // スライドの表示枚数
-  autoplay: { // 自動再生
-    delay: 1000, // 3秒後に次のスライド
-    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+  centeredSlides: true, 
+  loop: true, 
+  loopAdditionalSlides: 2,
+  
+  // スライド間の隙間を20pxにする
+  spaceBetween: 20,
+
+  speed: 1500, 
+  slidesPerView: 3.5, 
+  autoplay: { 
+    delay: 1000, 
+    disableOnInteraction: false, 
   },
-  // ページネーション
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  // 前後の矢印
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
