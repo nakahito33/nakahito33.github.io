698a89e57c8f4bb1852d77dd1c7706b244de35cd
meikaya <meikayana@icloud.com>
2025-12-06 12:53:18 +0900

    新しいcontents-1の完成

--- scripts/contents-1.js
+++ scripts/contents-1.js
@@ -1,44 +1,22 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // すべてのフォーム要素を取得
-  const forms = document.querySelectorAll('form');
-
-  // 各フォームにイベントリスナーを設定
-  forms.forEach(form => {
-    form.addEventListener('submit', (event) => {
-      event.preventDefault(); // デフォルトのフォーム送信を停止
-
-      // フォーム内の入力フィールドとエラーメッセージを取得
-      const emailInput = form.querySelector('input[type="email"]');
-      const passwordInput = form.querySelector('input[type="password"]');
-      const emailError = emailInput.nextElementSibling;
-      const passwordError = passwordInput.nextElementSibling;
-
-      // エラーメッセージの表示をリセット
-      emailError.style.display = 'none';
-      passwordError.style.display = 'none';
-      
-      let isValid = true;
-
-      // メールアドレスが空かチェック
-      if (emailInput.value.trim() === '') {
-        emailError.style.display = 'block';
-        isValid = false;
-      }
-      
-      // パスワードが空かチェック
-      if (passwordInput.value.trim() === '') {
-        passwordError.style.display = 'block';
-        isValid = false;
-      }
-
-      // すべての入力が有効であれば、ページを遷移
-      if (isValid) {
-        window.location.href = 'index.html';
-      }
-    });
-  });
+const swiper = new Swiper(".swiper", {
+  centeredSlides: true, // 1枚目のスライドを中央にする
+  loop: true, // ループさせる
+  speed: 1500, // 少しゆっくり(デフォルトは300)
+  slidesPerView: 3.5, // スライドの表示枚数
+  autoplay: { // 自動再生
+    delay: 1000, // 3秒後に次のスライド
+    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+  },
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
-// これいらなくなっちゃった
